b24d249590533b9a28c90e0ed0e2c40b6aea99f7
Build Wizard <>
2021-08-22 17:23:23 +0000

    Release Build 1.12-devbuilds-49

--- .github/workflows/publish/config/build.sh
+++ .github/workflows/publish/config/build.sh
@@ -3,7 +3,7 @@
 # Runs the build
 
 if [ "$BS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -md sha512 -pbkdf2 -iter 50000 -k "$BS_DECR_KEY_SECRET" -in keystore.jks.enc -out keystore.jks -d
+  openssl aes-256-cbc -md sha512 -pbkdf2 -iter 50000 -k "$BS_ENC_KEY_SECRET" -in keystore.jks.enc -out keystore.jks -d
 fi
 
 chmod +x gradlew
