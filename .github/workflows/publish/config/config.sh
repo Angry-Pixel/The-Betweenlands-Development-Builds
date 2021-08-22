af8d2e05fefd38ef78d8567d952fea1b355001da
Build Wizard <>
2021-08-22 19:29:13 +0000

    Development Build 1.12-devbuilds-57

--- .github/workflows/publish/config/config.sh
+++ .github/workflows/publish/config/config.sh
@@ -2,36 +2,38 @@
 
 # Sets up the necessary environment variables
 
-# Required
-
 echo "BS_DEPLOY_REPOSITORY_URI=git@github.com:Angry-Pixel/The-Betweenlands-Development-Builds.git" >> $GITHUB_ENV
 echo "BS_DEPLOY_REPOSITORY_BRANCH=master" >> $GITHUB_ENV
 echo "BS_DEPLOY_NAME=Build Wizard" >> $GITHUB_ENV
 echo "BS_ABORT_ON_DEPLOY_ERROR=true" >> $GITHUB_ENV
 
 if [ "$BS_IS_DEPLOYMENT" == 'false' ]; then
-  echo "BS_BUILD_TYPE=release" >> $GITHUB_ENV
-  echo "BS_BUILD_RELEASE=true" >> $GITHUB_ENV
-  echo "BS_BUILD_TITLE=Release Build ${GITHUB_REF##*/}-${GITHUB_RUN_NUMBER}" >> $GITHUB_ENV
-  echo "BS_BUILD_TAG=release-${BS_BUILD_BRANCH}-${BS_BUILD_NUMBER}-$(date +'%d.%m.%Y')" >> $GITHUB_ENV
-  echo "BS_BUILD_NOTES_FILE=release_notes" >> $GITHUB_ENV
+  echo "BS_BUILD_NOTES_FILE=build_notes" >> $GITHUB_ENV
+  
+  if [[ "$GITHUB_REF" == *"release"* ]]; then
+    echo "BS_BUILD_TYPE=release" >> $GITHUB_ENV
+    echo "BS_BUILD_RELEASE=true" >> $GITHUB_ENV
+    echo "BS_BUILD_TITLE=Release Build ${GITHUB_REF##*/}-${GITHUB_RUN_NUMBER}" >> $GITHUB_ENV
+    echo "BS_BUILD_TAG=release-${BS_BUILD_BRANCH}-${BS_BUILD_NUMBER}-$(date +'%d.%m.%Y')" >> $GITHUB_ENV
+  else
+    echo "BS_BUILD_TYPE=development" >> $GITHUB_ENV
+    echo "BS_BUILD_RELEASE=false" >> $GITHUB_ENV
+    echo "BS_BUILD_TITLE=Development Build ${GITHUB_REF##*/}-${GITHUB_RUN_NUMBER}" >> $GITHUB_ENV
+    echo "BS_BUILD_TAG=dev-${BS_BUILD_BRANCH}-${BS_BUILD_NUMBER}-$(date +'%d.%m.%Y')" >> $GITHUB_ENV
+  fi
+  
+  echo "Creating release notes"
+  git fetch --all --tags
+  previous_release_tag=$(git describe $(git rev-list --tags --max-count=1)^ --tags --abbrev=0 --match *-release)
+  echo "Creating list of changes since ${previous_release_tag}..."
+  echo "<details><summary>Changes</summary>" >> build_notes
+  git log ${previous_release_tag}..HEAD --since="$(git log -1 --format=%ai ${previous_release_tag})" --pretty=format:'%an, %ar (%ad):%n%B' --no-merges >> build_notes
+  echo "</details>" >> build_notes
+  cat build_notes
 fi
-
-# Optional
 
 if [ "$BS_PULL_REQUEST" == 'false' ]; then
   echo "DEPLOY_ENV=true" >> $GITHUB_ENV
   echo "DEPLOY_BUILD_TYPE=release" >> $GITHUB_ENV
   echo "DEPLOY_BUILD_NUMBER=$GITHUB_RUN_NUMBER" >> $GITHUB_ENV
-fi
-
-if [ "$BS_IS_DEPLOYMENT" == 'false' ]; then
-  echo "Creating release notes"
-  git fetch --all --tags
-  previous_release_tag=$(git describe $(git rev-list --tags --max-count=1)^ --tags --abbrev=0 --match *-release)
-  echo "Creating list of changes since ${previous_release_tag}..."
-  echo "<details><summary>Changes</summary>" >> release_notes
-  git log ${previous_release_tag}..HEAD --since="$(git log -1 --format=%ai ${previous_release_tag})" --pretty=format:'%an, %ar (%ad):%n%B' --no-merges >> release_notes
-  echo "</details>" >> release_notes
-  cat release_notes
 fi
--- .github/workflows/publish/setup.sh
+++ .github/workflows/publish/setup.sh
@@ -4,6 +4,18 @@
 
 echo "Running setup"
 
+export BS_DEPLOY_REPOSITORY_URI='git@github.com'
+echo "BS_DEPLOY_REPOSITORY_URI=${BS_DEPLOY_REPOSITORY_URI}" >> $GITHUB_ENV
+
+export BS_DEPLOY_REPOSITORY_BRANCH='master'
+echo "BS_DEPLOY_REPOSITORY_BRANCH=${BS_DEPLOY_REPOSITORY_BRANCH}" >> $GITHUB_ENV
+
+export BS_DEPLOY_NAME='GitHub Actions'
+echo "BS_DEPLOY_NAME=${BS_DEPLOY_NAME}" >> $GITHUB_ENV
+
+export BS_ABORT_ON_DEPLOY_ERROR='true'
+echo "BS_ABORT_ON_DEPLOY_ERROR=${BS_ABORT_ON_DEPLOY_ERROR}" >> $GITHUB_ENV
+
 if [ -f "build_config" ]; then
 
   echo "Loading build config"
@@ -65,6 +77,21 @@ else
     return 1
   fi
 
+  export BS_BUILD_TYPE='release'
+  echo "BS_BUILD_TYPE=${BS_BUILD_TYPE}" >> $GITHUB_ENV
+  
+  export BS_BUILD_RELEASE='true'
+  echo "BS_BUILD_RELEASE=${BS_BUILD_RELEASE}" >> $GITHUB_ENV
+  
+  export BS_BUILD_TITLE="Build ${GITHUB_REF##*/}-${GITHUB_RUN_NUMBER}"
+  echo "BS_BUILD_TITLE=${BS_BUILD_TITLE}" >> $GITHUB_ENV
+  
+  export BS_BUILD_TAG="${BS_BUILD_BRANCH}-${BS_BUILD_NUMBER}-$(date +'%d.%m.%Y')"
+  echo "BS_BUILD_TAG=${BS_BUILD_TAG}" >> $GITHUB_ENV
+  
+  export BS_BUILD_NOTES_FILE='build_notes'
+  echo "BS_BUILD_NOTES_FILE=${BS_BUILD_NOTES_FILE}" >> $GITHUB_ENV
+  
   export BS_BUILD_NUMBER=${GITHUB_RUN_NUMBER}
   echo "BS_BUILD_NUMBER=${BS_BUILD_NUMBER}" >> $GITHUB_ENV
   
