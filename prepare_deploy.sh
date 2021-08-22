48fa0213c9cb1d9bd4b0ea0f33f14be1e7276b65
Build Wizard <>
2021-08-22 14:09:58 +0000

    Release Build 1.12-devbuilds-35

--- prepare_deploy.sh
+++
@@ -1,7 +0,0 @@
-git config --local user.name "Travis Build Wizard"
-git config --local user.email "<>"
-if [ "$DEPLOY_BUILD_TYPE" == "release" ]; then
-  git tag "release-${DEPLOY_BUILD_BRANCH}-${DEPLOY_BUILD_NUMBER}-$(date +'%d.%m.%Y')"
-else
-  git tag "dev-${DEPLOY_BUILD_BRANCH}-${DEPLOY_BUILD_NUMBER}-$(date +'%d.%m.%Y')"
-fi
